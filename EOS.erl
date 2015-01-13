f5b706708affe535e698eef4843b822572baed21
Gregory Cremins <coolwhar@gmail.com>
2015-01-13 14:03:00 -0500

    Laptop is working, so im copying everything off of it in case of failure.

--- EOS.erl
+++ EOS.erl
@@ -1,13 +1,11 @@
 -module(eos).
 -export([startOS/0, main/0]).
 startOS() ->
-  eos:main(),
   %spawn memory process
   MEM_PID = spawn(memory, running, []),
-  MEM_PID ! startup.
-
+  MEM_PID ! startup,
+  eos:main().
 main() -> 
-
 X = io:get_line("> "),
 LEN = string:len(X) - 1,
 CI = string:substr(X, 1, LEN),
--- memory.erl
+++ memory.erl
@@ -1,28 +1,41 @@
 -module(memory).
--export([running/0]).
+-export([running/0, memCommandLoop/1]).
 running() ->
 	io:fwrite("Memory started"),
 	MEM = dict:new(),
-	MEM_0 = (memory, [], MEM),
-	memCommandLoop(MEM_0),
-	io:fwrite("Memory has ended, thank you for your business"),
-	end;
-memCommmandLoop(dict) ->
+	memCommandLoop(MEM),
+	io:fwrite("Memory has ended, thank you for your business").
+memCommandLoop(dict) when is_tuple(dict) andalso element(1, dict) =:= dict ->
 	receive
 		load ->
-			STRIPPED = string:(msg, both, $\s)
-			if
-				string:length(STRIPPED) rem 2 == 0 ->
-					memory:load(msg, dict),
-					memCommandLoop();
+			STRIPPED = string:strip(msg, both, $\s),
+			NEWMEM = memory:load(STRIPPED,dic),
+			memory:memCommandLoop(NEWMEM);
 		show ->
-			memory:show(dict),
-			memCommandLoop();
+			memory:show(dic),
+			memory:memCommandLoop(dic);
 		shutdown ->
-			end();
-load(msg, dict) ->
-	INDEX = string:chr(msg, $\s),
-	HEX = string:substring(msg, 0, index);
+			io:fwrite("Memory shutting down")
+	end,
+	io:fwrite("thingy");
+memCommandLoop(_) ->
+	io:fwrite("POOP").
+
+	
+load(msg, dic) ->
+	X = string:length(msg) rem 2,
+	if
+		X == 0 ->
+			HEX = string:substring(msg, 0, index),
+			dict:append(memory, HEX, dic);
+			
+		true ->
+			io:fwrite("Error in load")
+	end.
+	
+show(dic) ->
+	MEMORY = dict:fetch(memory, dic),
+	io:fwrite(MEMORY + "").
 	
 	
 			
